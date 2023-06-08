fd3127dd4134c85d5b46c96189820e8ea06fe828
“Medoelsheta1” <elsheta474@gmail.com>
2023-06-08 20:16:51 +0300

    bootstrab-modifation2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {persistor, store} from './Components/RTK/store';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
